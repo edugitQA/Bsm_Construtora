b5736b466d8357700dd6c89b173a19ac04fcee2b
eduardo Alves <eduardo.telecomrbs@gmail.com>
2023-07-19 20:37:44 -0300

    Bsm_Leitura_Botões

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Bsm_Construtora
-cadastro de Funcionários
+# cadastro de Funcionários empresa Bsm Construtora
+
+----------------------------------------------------------------------------------------------------------
+
+interface sendo criada para realizar organização das atividades diárias de funcionários da empresa Bsm Construtora,
+com intuito de registrar quantidade de metros de trabalho realizado diariamente, juntamente com data, e também o tipo de atividade realizada por determinado funcionáriotutam, e futuramente com imprensão de relatório mensal de resultados.
+
+-----------------------------------------------------------------------------------------------------------
+
+-METAS:
+*Criação do front-end/back-end com todas funcionalidades e interações; 
+*Um banco de dados com capacidade pra armazenamento de dados mensal;
+*Página de loguin para proteger possíveis dados que serão implementados futuramente como número de cpf, pix, nome completo de funcionários;(LGPD)
+*Possibilidade de criação de uma funcionalidade de impressão de resultados mensais de funcionários, futuramente podendo implementar um possível contra-cheque;
+*Implementar cadastro de mais um usuário do sistema autorizado pelo administrador;
+
+-----------------------------------------------------------------------------------------------------------
+
+Autores do da criação do projeto:
+Eduardo Alves de Paulo Ferreira(Front-end/banco de dados)
+Cleberson Duarte(back-end/banco de dados)
+Henrique Medeiros(back-end/banco de dados)
+
--- script.js
+++ script.js
@@ -1,7 +1,58 @@
-class Bsm {
-    constructor(){
-        this.id = 1;
-        this.arrayFuncionarioBsm = []
-        this.ediId = null;
-    }
+//ler e obter todos dados,
+//nome do funcionário
+//dados do select
+//dados do calendário
+//dados de metragem realizada
+
+//funções a serem executadas com respectivos dados*/
+
+
+
+
+//ler nome do funcionário
+const lineInput = document.querySelector('#form');
+const nomeFuncionario = document.querySelector('.funcionarioBsm')
+
+
+lineInput.addEventListener('submit', function(event){
+  event.preventDefault()
+  const input = nomeFuncionario.value
+    console.log(input)
+})
+
+//ler dados do select
+function atualizaSelectProfissao(){
+  let select = document.querySelector('#profissaoBsm');
+  let optionValue= select.options[select.selectedIndex];
+  let value = optionValue.value;
+  let text = optionValue.text;
+
+ 
+  console.log(value,text)
 }
+atualizaSelectProfissao()
+
+function atualizaSelectServico(){
+  let select = document.querySelector('#servicoBsm');
+  let optionValue= select.options[select.selectedIndex];
+  
+  let value = optionValue.value;
+  let text = optionValue.text;
+
+  console.log(value,text);  
+}
+atualizaSelectServico()
+ 
+/* 
+function atualizaSelect(elementId1,elementId2) {
+  let select = document.querySelector(elementId1, elementId2);
+  let optionValue = select.options[select.selectedIndex];
+  let value = optionValue.value;
+  let text = optionValue.text;
+
+  console.log(value, text);
+} 
+
+atualizaSelect('#profissaoBsm');
+atualizaSelect('#servicoBsm');
+ */
